f164917d23cd9b6040c15f51c836440050ab1c2f
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-01-31 17:26:28 +0700

    solve TwoSum

--- d.js
+++ d.js
@@ -1,22 +1,37 @@
-function isValid(str) {
-    let Stack = []
-    let mapOposite ={
-        '(':')',
-        '[':']',
-        '{':'}'
-    }
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
-            Stack.push(str[i])
-        } else {
-            let last = Stack.pop()
-            if (str[i] !== mapOposite[last]) return false
-        }
-    }
-    if (Stack.length !== 0) return false
-    return true
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-console.log(isValid('()')); // true
-console.log(isValid('()[]{}')); // true
-console.log(isValid('(]')); // false
+function depthFirstValues(root) {
+  if (!root) return [];
+  const stack = [root];
+  const values = [];
+  while (stack.length) {
+    const node = stack.pop();
+    values.push(node.value);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return values;
+}
+  
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+console.log(a)
+
+console.log(depthFirstValues(a));
