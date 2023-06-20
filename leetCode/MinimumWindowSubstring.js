c5c05bfab08c7e488654865872464f0cc5deecdb
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-06-21 01:23:40 +0700

    finised minimum window substring

--- leetCode/MinimumWindowSubstring.js
+++ leetCode/MinimumWindowSubstring.js
@@ -1,3 +1,33 @@
+// 76. Minimum Window Substring
+// Hard
+// 15.2K
+// 639
+// Companies
+// Given two strings s and t of lengths m and n respectively, return the minimum window 
+// substring
+//  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+// The testcases will be generated such that the answer is unique.
+
+ 
+
+// Example 1:
+
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+// Example 2:
+
+// Input: s = "a", t = "a"
+// Output: "a"
+// Explanation: The entire string s is the minimum window.
+// Example 3:
+
+// Input: s = "a", t = "aa"
+// Output: ""
+// Explanation: Both 'a's from t must be included in the window.
+// Since the largest window of s only has one 'a', return empty string.
+ 
 
 
 var minWindow = (s, t) =>　{
@@ -78,5 +108,6 @@ const subtractLeftFrequency = (s, left, frequencyMap, matched) => {
 const getSubString = (s, start, end) => end <= s.length ? s.substring(start, start + end) : '';
 
 
-console.log(minWindow("ADOBECODEBANC", "ABC")); 
-
+console.log(minWindow("ADOBECODEBANC", "ABC")); // BANC
+console.log(minWindow("a", "a")); // a
+console.log(minWindow("a", "aa")); // ''
