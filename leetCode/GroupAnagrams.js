2c7ee2f80b3a4e00eb913b0f42e5a85b9bf55647
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-10-20 23:16:42 +0700

    update

--- leetCode/GroupAnagrams.js
+++ leetCode/GroupAnagrams.js
@@ -52,7 +52,7 @@ function groupAnagrams(words, map = new Map()) {
 
 }
 
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); // [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+// console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); // [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
 // console.log(groupAnagrams([""])); // [['']]
 // console.log(groupAnagrams(["a"])); // [['a']]
 
