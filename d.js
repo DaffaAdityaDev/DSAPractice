6a7a4810ac01dcb8c8b9e3acb5b33f3281aab301
DaffaAdityaDev <daffaaditya990@gmail.com>
2024-02-05 01:11:39 +0700

    solve combination Sum

--- d.js
+++ d.js
@@ -1,34 +1,5 @@
-function VigenèreCipher(key, abc) {
-    this.encode = function (str) {
-        let cipherText = '';
-        for (let i = 0; i < str.length; i++) {
-            let row = abc.indexOf(key[i % key.length]);
-            let col = abc.indexOf(str[i]);
-            if (abc.indexOf(col) >= 0) {
-                cipherText += abc[(row + col) % abc.length];
-            } else {
-                cipherText += str[i];
-            }
-        }
-        return str === str.toUpperCase() ? cipherText.toUpperCase() : cipherText;
-    }
-        
-    this.decode = function (str) {
-        let deCipherText = '';
-        for (let i = 0; i < str.length; i++) {
-            let row = abc.indexOf(key[i % key.length]);
-            let col = abc.indexOf(str[i]);
-            if (abc.indexOf(col) >= 0) {
-                deCipherText += abc[(col - row + abc.length) % abc.length];
-            } else {
-                deCipherText += str[i];
-            }
-        }
-        return str === str.toUpperCase() ? deCipherText.toUpperCase() : deCipherText;
-    };
+function spiralize (n) {
+	return [];
 }
 
-console.log(new VigenèreCipher('password', 'abcdefghijklmnopqrstuvwxyz').encode('codewars')); // returns 'rovwsoiv'
-console.log(new VigenèreCipher('password', 'abcdefghijklmnopqrstuvwxyz').decode('rovwsoiv')); // returns 'codewars'
-console.log(new VigenèreCipher('password', 'abcdefghijklmnopqrstuvwxyz').encode('CODEWARS')); // returns 'ROVWSOIV'
-console.log(new VigenèreCipher('password', 'abcdefghijklmnopqrstuvwxyz').decode('ROVWSOIV')); // returns 'CODEWARS'
+console.log(spiralize(5)); // [ [1, 1, 1, 1, 1], [0, 0, 0, 0, 1], [1, 1, 1, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1] ]
