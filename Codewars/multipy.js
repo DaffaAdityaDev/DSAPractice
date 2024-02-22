3c9820ad6358ca572d41332d86e41dbf605f8bac
DaffaAdityaDev <daffaaditya990@gmail.com>
2024-02-22 18:49:48 +0700

    solve mutipy

---
+++ Codewars/multipy.js
@@ -0,0 +1,12 @@
+// This code does not execute properly. Try to figure out why.
+
+// DEBUGGINGFUNDAMENTALS
+
+function multiply(a, b){
+    return a * b
+}
+
+
+console.log(multiply(2, 3)) // 6
+console.log(multiply(2, 3)) // 6
+console.log(multiply(2, 3)) // 6
