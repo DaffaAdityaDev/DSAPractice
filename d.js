1c9f463a38f30b80a6136041d3da636146f31ead
DaffaAdityaDev <daffaaditya990@gmail.com>
2024-04-10 10:51:04 +0700

    solve Who like it

--- d.js
+++ d.js
@@ -1,10 +1,23 @@
-function removeChar(str){
-  //You got this!
-  return str.slice(1, -1);
-};
+function likes(names) {
+  // TODO
+  if (names.length === 0) return 'no one likes this'
+  let peopleLike = ''
 
-console.log(removeChar('eloquent')); // 'loquen'
-console.log(removeChar('country')); // 'ountr'
- 
- 
- 
+  if (names.length === 1) {
+    peopleLike = names[0] + ' likes this'
+  }
+  if (names.length === 2) {
+    peopleLike = names[0] + ' and ' + names[1] + ' like this'
+  }
+  if (names.length === 3) {
+    peopleLike = names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'
+  }
+
+  if (names.length > 3) {
+    peopleLike = names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this'
+  }
+
+  return peopleLike
+  
+}
+
