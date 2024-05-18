44b1bb5665e595c2f376ddff9ad1bf4c52f82942
daffaadityadev <daffaaditya990@gmail.com>
2024-05-18 21:17:29 +0700

    update

--- d.js
+++ d.js
@@ -1,22 +1,4 @@
-function climbingLeaderboard(ranked, player) {
-    // Write your code here
-    let uniqueRanked = [... new Set(ranked)]
-    let result = []
-    let index = uniqueRanked.length - 1
-    player.forEach(p => {
-        while (index >= 0) {
-            if (p >= uniqueRanked[index]) {
-                index--
-            } else {
-                result.push(index + 2)
-                break
-            }
-        }
-        if (index < 0) {
-            result.push(1)
-        }
-    })
-    return result
+function hurdleRace(k, height) {
+    let max = Math.max(...height);
+    return max > k ? max - k : 0;
 }
-
-console.log(climbingLeaderboard([100, 90, 90, 80], [70, 80, 105])) // [4, 3, 1]
