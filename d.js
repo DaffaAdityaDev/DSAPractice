4caceea57fcca8356671e258ce9005041e3cf0ad
daffaadityadev <daffaaditya990@gmail.com>
2024-05-20 19:45:21 +0700

    update

--- d.js
+++ d.js
@@ -1,4 +1,15 @@
-function hurdleRace(k, height) {
-    let max = Math.max(...height);
-    return max > k ? max - k : 0;
+function increamParlor(m, arr) {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === m) {
+            result.push(i + 1, j + 1)
+        }
+        }
+    }
+    return result
+  
 }
+
+console.log(increamParlor(4, [1, 4, 5, 3, 2])) // [1, 4]
+console.log(increamParlor(4, [2, 2, 4, 3])) // [1, 2]
