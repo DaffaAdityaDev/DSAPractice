e441487a251db1087b7db6a6d2c84bfe6edc0fe8
daffaadityadev <daffaaditya990@gmail.com>
2024-05-30 19:11:32 +0700

    update

--- d.js
+++ d.js
@@ -1,15 +1,8 @@
-function increamParlor(m, arr) {
-    let result = []
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-        if (arr[i] + arr[j] === m) {
-            result.push(i + 1, j + 1)
-        }
-        }
-    }
-    return result
-  
+function(d) {
+
+    let removeSpace = 
 }
 
-console.log(increamParlor(4, [1, 4, 5, 3, 2])) // [1, 4]
-console.log(increamParlor(4, [2, 2, 4, 3])) // [1, 2]
+
+console.log(d('Hello,       wWorld!'))
+
