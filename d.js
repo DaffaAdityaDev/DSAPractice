93aa239a13ad80ea864bc82bd21dfacffe6d44a4
daffaadityadev <daffaaditya990@gmail.com>
2024-07-13 20:23:12 +0700

    solve staircae

--- d.js
+++ d.js
@@ -1,8 +1,14 @@
-function(d) {
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let x = grid.length;
+    let y = grid[0].length;
+    let currentIsland = 0;
+    let 
+};
 
-    let removeSpace = 
-}
-
-
-console.log(d('Hello,  wWorld!'))
-
+console.log(numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]) === 1)
+// console.log(numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]) === 3)
+// console.log(numIslands([["1","0","1","1","0","1","1"]]) === 3)
