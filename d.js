7b57c30b3d0fd3bf80913b1287ff02e45f1af820
daffaadityadev <daffaaditya990@gmail.com>
2024-07-29 06:16:03 +0700

    solve friend or foe

--- d.js
+++ d.js
@@ -1,18 +1,7 @@
-// Return the number (count) of vowels in the given string.
-
-// We will consider a, e, i, o, u as vowels for this Kata (but not y).
-
-// The input string will only consist of lower case letters and/or spaces.
-
-function getCount(str) {
-    let Vowel = ["a", "e", "i", "o", "u"];
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (Vowel.includes(str[i])) {
-            count++;
-        }
-    }
-    return count;
+function friend(friends){
+    //your code here
+    return friends.filter(friend => friend.length === 4)
 }
 
-console.log(getCount("abracadabra")); // 5
+
+console.log(friend(["Ryan", "Kieran", "Mark"])) // ["Ryan", "Mark"]
