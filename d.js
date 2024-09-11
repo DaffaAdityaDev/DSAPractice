a398ced2a4daab6348177134ca2e4997ea490437
daffaadityadev <daffaaditya990@gmail.com>
2024-09-11 21:25:21 +0700

    update

--- d.js
+++ d.js
@@ -1,9 +1,6 @@
-function alphabetPosition(text) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    return text.toLowerCase().split('')
-        .filter(char => alphabet.includes(char))
-        .map(char => alphabet.indexOf(char) + 1)
-        .join(' ');
-}
-
-console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11
+export const getAnAdult = (users) => {
+    // Write your code here
+    return users.find(user => user.age >= 18)
+  }
+  
+console.log(getAnAdult([{name: "John", age: 20}, {name: "Jane", age: 18}, {name: "Jim", age: 22}]))
