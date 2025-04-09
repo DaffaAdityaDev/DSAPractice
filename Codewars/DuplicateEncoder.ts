08968f7a7213065758e034607a05692b26d53879
daffaadityadev <daffaaditya990@gmail.com>
2025-04-09 23:02:19 +0700

    adjust

--- Codewars/DuplicateEncoder.ts
+++ Codewars/DuplicateEncoder.ts
@@ -7,28 +7,24 @@
 // "(( @"     =>  "))((" 
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
-
-export function duplicateEncode(word: string) {
-  const lowerCaseWord = word.toLowerCase();
-  const hashMap = new Map<string, number>();
-  let result = '';
-
-  for (const char of lowerCaseWord) {
-    hashMap.set(char, (hashMap.get(char) ?? 0) + 1);
-  }
-
+export function duplicateEncode(str: string) {
+  const lowerCaseWord : any = str.toLowerCase()
+  const hashMap = new Map()
+  let result = ''
+  
 
   for (const char of lowerCaseWord) {
-    result += hashMap.get(char) === 1 ? '(' : ')';
+    hashMap.set(char, (hashMap.get(char) ?? 0) + 1)
   }
 
-  return result;
+  for (const char of lowerCaseWord) {
+    result += hashMap.get(char) === 1 ? "(" : ")"
+  }
+
+  return result
 }
 
 console.log(duplicateEncode("din")) // "((("
 console.log(duplicateEncode("recede")) // "()()()"
 console.log(duplicateEncode("Success")) // ")())())"
-console.log(duplicateEncode("(( @")) // "))(("
-
-
-
+console.log(duplicateEncode("(( @")) // "))((" 
